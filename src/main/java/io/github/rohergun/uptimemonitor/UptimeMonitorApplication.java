37939253b83f1850d45d3d05fe834357e2fae00e
RohatErgun <rohatergun06@gmail.com>
2026-04-27 18:51:52 +0200

    First commit

---
+++ src/main/java/io/github/rohergun/uptimemonitor/UptimeMonitorApplication.java
@@ -0,0 +1,13 @@
+package io.github.rohergun.uptimemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimeMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rohergun/uptimemonitor/UptimeMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rohergun.uptimemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
