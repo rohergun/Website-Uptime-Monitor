32373297f0253cbf38c9e9f6b556000bc5edf889
RohatErgun <rohatergun06@gmail.com>
2026-04-28 11:01:29 +0200

    feat(entity): add MonitoredSite

--- src/main/java/io/github/rohergun/uptimemonitor/model/entity/MonitoredSite.java
+++ src/main/java/io/github/rohergun/uptimemonitor/model/entity/MonitoredSite.java
@@ -1,2 +1,41 @@
-package io.github.rohergun.uptimemonitor.model.entity;public class MonitoredSite {
+package io.github.rohergun.uptimemonitor.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "sites")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MonitoredSite {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private int checkIntervalsPerMinute;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
 }
