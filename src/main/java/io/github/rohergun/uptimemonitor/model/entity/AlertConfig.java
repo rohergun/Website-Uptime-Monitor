fea1faa585eb5e0502a8ad965536ca60cb8a5577
RohatErgun <rohatergun06@gmail.com>
2026-04-28 11:15:34 +0200

    feat(entity): add AlertConfig

--- src/main/java/io/github/rohergun/uptimemonitor/model/entity/AlertConfig.java
+++ src/main/java/io/github/rohergun/uptimemonitor/model/entity/AlertConfig.java
@@ -1,2 +1,46 @@
-package io.github.rohergun.uptimemonitor.model.entity;public class AlertConfig {
+package io.github.rohergun.uptimemonitor.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "alerts")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AlertConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Builder.Default
+    private int cooldownMinutes = 60;
+
+    @Builder.Default
+    private boolean emailEnabled = true;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    // Todo FK site_id
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
 }
