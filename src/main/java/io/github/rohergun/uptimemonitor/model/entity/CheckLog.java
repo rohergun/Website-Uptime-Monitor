63e4a11f146d875afe76bdc9b31ae27f630a15ca
RohatErgun <rohatergun06@gmail.com>
2026-04-28 11:24:55 +0200

    feat(entity): add CheckLog

--- src/main/java/io/github/rohergun/uptimemonitor/model/entity/CheckLog.java
+++ src/main/java/io/github/rohergun/uptimemonitor/model/entity/CheckLog.java
@@ -1,2 +1,39 @@
-package io.github.rohergun.uptimemonitor.model.entity;public class CheckLog {
+package io.github.rohergun.uptimemonitor.model.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "checklogs")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CheckLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private int httpStatus;
+
+    private boolean success;
+
+    private int responseTime;
+
+    private String errorMessage;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // Todo FK monitoredSite_id
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
 }
